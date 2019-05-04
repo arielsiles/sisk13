35af942da54b68b7190cffe4a4ed911d7c1bb1bd
Borboton <borboton23@gmail.com>
2019-05-04 00:15:14 -0400

    04.05.2019 0015

---
+++ src/main/com/encens/khipus/action/production/MaterialInputAction.java
@@ -0,0 +1,42 @@
+package com.encens.khipus.action.production;
+
+import com.encens.khipus.framework.action.GenericAction;
+import com.encens.khipus.model.production.MaterialInput;
+import com.encens.khipus.model.warehouse.ProductItem;
+import org.jboss.seam.ScopeType;
+import org.jboss.seam.annotations.Factory;
+import org.jboss.seam.annotations.Name;
+import org.jboss.seam.annotations.Scope;
+
+
+@Name("materialInputAction")
+@Scope(ScopeType.CONVERSATION)
+public class MaterialInputAction extends GenericAction<MaterialInput> {
+
+    @Factory(value = "materialInput", scope = ScopeType.STATELESS)
+    public MaterialInput initMaterialInput() {
+        return getInstance();
+    }
+
+    /*@Override
+    @Begin(ifOutcome = Outcome.SUCCESS, flushMode = FlushModeType.MANUAL)
+    public String select(Formulation instance) {
+        String outCome = super.select(instance);
+        setOp(OP_UPDATE);
+        setFormulationInputList(getInstance().getFormulationInputList());
+        return outCome;
+    }*/
+
+    /*@Override
+    public String update() {
+
+        formulationService.updateFormulation(getInstance(), formulationInputList);
+        return Outcome.SUCCESS;
+    }*/
+
+    public void assignProduct(ProductItem productItem){
+        getInstance().setProductItem(productItem);
+        getInstance().setProductItemCode(productItem.getProductItemCode());
+    }
+
+}
---
+++ src/main/com/encens/khipus/action/production/MaterialInputDataModel.java
@@ -0,0 +1,46 @@
+package com.encens.khipus.action.production;
+
+import com.encens.khipus.framework.action.QueryDataModel;
+import com.encens.khipus.model.production.MaterialInput;
+import org.jboss.seam.ScopeType;
+import org.jboss.seam.annotations.Create;
+import org.jboss.seam.annotations.Name;
+import org.jboss.seam.annotations.Scope;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ *
+ */
+@Name("materialInputDataModel")
+@Scope(ScopeType.PAGE)
+public class MaterialInputDataModel extends QueryDataModel<Long, MaterialInput> {
+
+    private String name;
+
+    private static final String[] RESTRICTIONS = {"lower(materialInput.productItem.name) like concat('%', concat(lower(#{materialInputDataModel.name}), '%'))"};
+
+    @Create
+    public void init() {
+        //sortProperty = "";
+    }
+
+    @Override
+    public String getEjbql() {
+        return "select materialInput from MaterialInput materialInput";
+    }
+
+    @Override
+    public List<String> getRestrictions() {
+        return Arrays.asList(RESTRICTIONS);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
