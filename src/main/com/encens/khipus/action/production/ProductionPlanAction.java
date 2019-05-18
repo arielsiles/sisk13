c9736b8f862e9b9f297b84c1a5061eab9ab69f1c
AS <arielrse@gmail.com>
2019-05-18 11:54:55 -0400

    18.05.2019 production

--- src/main/com/encens/khipus/action/production/ProductionPlanAction.java
+++ src/main/com/encens/khipus/action/production/ProductionPlanAction.java
@@ -2,13 +2,18 @@ package com.encens.khipus.action.production;
 
 import com.encens.khipus.framework.action.GenericAction;
 import com.encens.khipus.framework.action.Outcome;
+import com.encens.khipus.model.production.Production;
 import com.encens.khipus.model.production.ProductionPlan;
 import com.encens.khipus.model.production.ProductionProduct;
+import com.encens.khipus.model.production.Supply;
 import com.encens.khipus.model.warehouse.ProductItem;
 import com.encens.khipus.service.production.ProductionPlanService;
+import com.encens.khipus.util.BigDecimalUtil;
+import com.encens.khipus.util.Constants;
 import org.jboss.seam.ScopeType;
 import org.jboss.seam.annotations.*;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -86,6 +91,19 @@ public class ProductionPlanAction extends GenericAction<ProductionPlan> {
             result = "SI";
 
         return  result;
+    }
+
+    public BigDecimal getDestinedMilk(Production production){
+
+        BigDecimal result = BigDecimal.ZERO;
+
+        for (Supply supply : production.getSupplyList()){
+            if (supply.getProductItemCode().equals(Constants.ID_ART_RAW_MILK))
+                result = BigDecimalUtil.sum(result, supply.getQuantity(), 2);
+        }
+
+        return result;
+
     }
 
     public double getTotalWeighed(ProductionPlan productionPlan){
