b5e27479e5f49e1d7396fbb807b4d55018975b27
Borboton <borboton23@gmail.com>
2019-06-13 22:22:49 -0400

    13.06.2019 2222

--- query/query_v3.4.5.sql
+++ query/query_v3.4.5.sql
@@ -1,10 +1,19 @@
 /** 13.06.2019 **/
-alter table periodocostoindirecto add column contab int(1) after procesado;
-update periodocostoindirecto p set p.`contab` = 1;
+ALTER TABLE periodocostoindirecto ADD COLUMN contab INT(1) AFTER procesado;
+UPDATE periodocostoindirecto p SET p.`contab` = 1;
 
-update periodocostoindirecto p set p.`contab` = 0 
-where p.`mes` = 5 and p.`idgestion` = 6;
+UPDATE periodocostoindirecto p SET p.`contab` = 0 
+WHERE p.`mes` = 5 AND p.`idgestion` = 6;
 
-update periodocostoindirecto p set p.`procesado` = 1;
-update periodocostoindirecto p set p.`procesado` = 0 
-where p.`mes` = 5 and p.`idgestion` = 6;
+UPDATE periodocostoindirecto p SET p.`procesado` = 1;
+UPDATE periodocostoindirecto p SET p.`procesado` = 0 
+WHERE p.`mes` = 5 AND p.`idgestion` = 6;
+
+--
+
+UPDATE pr_producto p SET p.`costo` = 0 WHERE p.`costo` IS NULL;
+
+UPDATE pr_producto p SET p.`costo_a` = 0 WHERE p.`costo_a` IS NULL;
+UPDATE pr_producto p SET p.`costo_b` = 0 WHERE p.`costo_b` IS NULL;
+UPDATE pr_producto p SET p.`costo_c` = 0 WHERE p.`costo_c` IS NULL;
+UPDATE pr_producto p SET p.`costouni` = 0 WHERE p.`costouni` IS NULL;
