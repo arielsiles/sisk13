ff45078cdacbe65ed3efb4a7b0b8a823202d86cd
Ariel S <ariel.siles@ilvabolivia.com>
2019-06-19 16:32:22 -0400

    19.06.2019 production

--- src/main/com/encens/khipus/action/production/ProductionPlanAction.java
+++ src/main/com/encens/khipus/action/production/ProductionPlanAction.java
@@ -506,6 +506,19 @@ public class ProductionPlanAction extends GenericAction<ProductionPlan> {
 
     }
 
+    public List<ProductionProduct> getProductionProductList(ProductionPlan productionPlan){
+
+        List<ProductionProduct> productionProductList = new ArrayList<ProductionProduct>();
+        System.out.println("=====> Plan: " + productionPlan.getDate());
+        for (ProductionProduct product : productionPlan.getProductionProductList()){
+            System.out.println("---------->>>> Plan Product:" + productionPlan.getDate() + " - " + product.getProductItem().getFullName() + " - " + product.getQuantity());
+            productionProductList.add(product);
+        }
+
+
+        return productionProductList;
+    }
+
     public BigDecimal getTotalIndirectCost() {
         return totalIndirectCost;
     }
