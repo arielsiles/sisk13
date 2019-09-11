c5dc2000b8f4f76ed020222d86ca9bbcba215120
AS <arielrse@gmail.com>
2019-09-11 16:24:59 -0400

    11.09.2019 1624

--- query/query_v3.6.7.sql
+++ query/query_v3.6.7.sql
@@ -1,2 +1,26 @@
 /** 09.09.2019 **/
-update arcgms a set a.`exije_cc` = 'N';
+UPDATE arcgms a SET a.`exije_cc` = 'N';
+
+/** 11.09.2019 **/
+-- PARA F. CISC
+DELETE FROM inv_subgrupos;
+DELETE FROM inv_grupos;
+DELETE FROM ventaarticulo;
+DELETE FROM articulospromocion;
+DELETE FROM promocion;
+DELETE FROM inv_articulos;
+
+DELETE FROM inv_almacenes;
+
+
+UPDATE gensecuencia g SET g.`valor` = 0 WHERE g.`nombre` = 'WAREHOUSE_GROUP_SEQUENCE';
+UPDATE gensecuencia g SET g.`valor` = 0 WHERE g.`nombre` = 'WAREHOUSE_PRODUCT_ITEM_SEQUENCE';
+
+DELETE FROM gensecuencia WHERE idgensecuencia = 1;
+DELETE FROM gensecuencia WHERE idgensecuencia = 11;
+
+-- Asociar al GRUPO
+-- 5451010000 Otros Ingresos Operativos Diversos
+
+/** Ejecutado en F.Cisc **/
+
