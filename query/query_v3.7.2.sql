69f07eebe66c4d79bf977402120de4ad478d6e49
Ariel S <ariel.siles@ilvabolivia.com>
2019-10-29 16:27:15 -0400

    29.10.2019 1627

--- query/query_v3.7.2.sql
+++ query/query_v3.7.2.sql
@@ -3,3 +3,8 @@ alter table credito add column fechavence date after ultimopago;
 
 -- for FCisc
 -- update transaccioncredito t set t.`id_tmpenc` = null where t.`idtransaccioncredito` = 280;
+-- update credito c set c.`fechavence` = '2019-01-01';
+
+delete from transaccioncredito where idcredito = 80;
+delete from credito where idcredito = 80;
+--
