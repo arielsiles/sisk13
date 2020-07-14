2e8a030b75cf1e8fb71efa1abeb43eb2d444b841
Ariel S <ariel.siles@ilvabolivia.com>
2020-07-14 14:00:27 -0400

    creditos garantes

--- query/query_v5.0.3.sql
+++ query/query_v5.0.3.sql
@@ -6,7 +6,23 @@
 create table garante (
 	idgarante bigint(20) not null,
 	idsocio bigint(20) not null,
+	descripcion varchar(100),
+	version bigint(20),
+	idcompania bigint(20),
 	primary key (idgarante)
 );
 
 alter table garante add foreign key (idsocio) references socio (idsocio);
+alter table garante add foreign key (idcompania) references compania (idcompania);
+
+create table documentogarante(
+	iddocumentogarante bigint(20) not null,
+	descripcion varchar(255),
+	idgarante bigint(20) not null,
+	version bigint(20),
+	idcompania bigint(20),
+	primary key (iddocumentogarante)
+);
+
+alter table documentogarante add foreign key (idgarante) references garante (idgarante);
+alter table documentogarante add foreign key (idcompania) references compania (idcompania);
