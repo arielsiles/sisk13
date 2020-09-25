efa3a3faa23066332fcb3447060133ff58370c5d
Ariel Siles <ariel.siles@gmail.com>
2020-09-25 15:52:59 -0400

    Registro de garante para creditos ok

--- query/query_v5.1.0.sql
+++ query/query_v5.1.0.sql
@@ -1,11 +1,11 @@
 /** Query for v 5.1.0 **/
 --
 -- 23.09.2020
-alter table garante drop foreign key garante_ibfk_1;
-alter table garante drop column idsocio;
+-- alter table garante drop foreign key garante_ibfk_1;
+-- alter table garante drop column idsocio;
 
-alter table garante add column idpersona bigint(20) not null after idgarante;
-alter table garante add foreign key (idpersona) references persona (idpersona);
+-- alter table garante add column idpersona bigint(20) not null after idgarante;
+-- alter table garante add foreign key (idpersona) references persona (idpersona);
 
 alter table garante add column idcredito bigint(20) not null after idpersona;
 alter table garante add foreign key (idcredito) references credito (idcredito);
