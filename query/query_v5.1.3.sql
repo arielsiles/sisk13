0f7e32228b22e3e930e90316cbbadafc926a75fb
Ariel Siles <ariel.siles@gmail.com>
2020-12-16 15:52:45 -0400

    16.12.2020 Modificacion rapida para generacion de planilla de aguinaldos

--- query/query_v5.1.3.sql
+++ query/query_v5.1.3.sql
@@ -1,1 +1,5 @@
+/** 16.12.2020 **/
 insert into funcionalidad values (268, 'CHRISTMASNATIONALSUMMARYREPORT', null, 4, 1, 'menu.rrhh.reports.christmasNationalSummary', 1);  -- ejecutado ilva
+
+-- En tabla PLANILLAAGUINADO
+-- idplanillaaguinaldo se modifico a autoincrement (revisar corregir)
