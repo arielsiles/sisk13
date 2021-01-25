4a53005044ccd86785d424de53e75a7bdd49fc27
Borboton <borboton23@gmail.com>
2021-01-24 20:05:28 -0400

    24012021 2005

--- query/query_v5.1.4.sql
+++ query/query_v5.1.4.sql
@@ -1,5 +1,7 @@
 -- v5.1.4
 -- 23.01.2021
 -- Para diferimiento
-alter table credito add column montodif decimal(13,2)  default 0 after importe;
-alter table credito add column cuotadif decimal(13,2)  default 0 after cuota;
+alter table credito add column montodif decimal(13,2) not null default 0 after importe;
+alter table credito add column cuotadif decimal(13,2) not null default 0 after cuota;
+
+alter table transaccioncredito add column cuotadif decimal(13,2)  default 0 after interes;
