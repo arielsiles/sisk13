1e0cd1a162ec27c7bfab1495ffd7f1594502ba0f
Ariel S <ariel.siles@ilvabolivia.com>
2021-02-02 10:11:06 -0400

    Creditos, monto para diferimiento...

--- query/query_v5.1.4.sql
+++ query/query_v5.1.4.sql
@@ -5,3 +5,13 @@ alter table credito add column montodif decimal(13,2) not null default 0 after i
 alter table credito add column cuotadif decimal(13,2) not null default 0 after cuota;
 
 alter table transaccioncredito add column cuotadif decimal(13,2)  default 0 after interes;
+
+-- Resticcion de funcionalidad Creditos
+-- CREDIT_PAYMENT
+-- Functionality.customers.creditPayment
+insert into funcionalidad(idfuncionalidad, codigo, idmodulo, permiso, nombrerecurso, idcompania) 
+values(421, 'CREDIT_PAYMENT', 1, 3, 'Functionality.customers.creditPayment', 1);
+
+-- CREDIT_GUARANTOR Functionality.customers.creditGuarantor
+insert into funcionalidad(idfuncionalidad, codigo, idmodulo, permiso, nombrerecurso, idcompania) 
+values(422, 'CREDIT_GUARANTOR', 1, 15, 'Functionality.customers.creditGuarantor', 1);
