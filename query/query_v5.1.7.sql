1413e8476b266154ef826c3c442789703d91fb27
Ariel Siles <ariel.siles@gmail.com>
2021-04-21 16:04:52 -0400

    Kardex clientes Venta sin factura

--- query/query_v5.1.7.sql
+++ query/query_v5.1.7.sql
@@ -3,3 +3,5 @@ alter table inv_articulos modify column saldo_mon decimal(20,6) null;
 alter table inv_articulos modify column ct decimal(20,6) null;
 
 -- 21.04.2021
+alter table tipocaja add column cuentacontingencia varchar(20) null after cuentaxcobrar;
+update tipocaja set cuentacontingencia = '2570110000' where idtipocaja in (1,2,3);
