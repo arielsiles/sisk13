bee3557dae0cb67545232b5327971bf60c5055e1
Ariel Siles <ariel.siles@gmail.com>
2021-04-29 15:47:06 -0400

    ventas productos, mas vendidos por tipo de caja...

--- query/query_v5.2.0.sql
+++ query/query_v5.2.0.sql
@@ -1,5 +1,15 @@
 -- 27.04.2021
-alter table categoriacliente add column idtipocaja bigint(20)after tipo;
-alter table categoriacliente add foreign key (idtipocaja) references tipocaja (idtipocaja);
+-- alter table categoriacliente add column idtipocaja bigint(20)after tipo;
+-- alter table categoriacliente add foreign key (idtipocaja) references tipocaja (idtipocaja);
 
 alter table tipocaja add column idcategoriacliente bigint(20)after no_cia;
+
+-- Configuraciones para release
+-- 0. Crear Almacen Agencia y asignar cuenta contable (1510110203 - Productos Agencia Central)
+-- 1. Crear Categorias Cliente con los productos y precios para fijar como los mas vendidos
+-- 1.1. En categoriacliente fijar tipo segun corresponde:
+--	FACTORY_LIST *
+--	AGENCY_LIST *
+--	VET_LIST
+
+-- 2. Relacionar en tipo de caja (tipocaja) con la categoriacliente q corresponde
