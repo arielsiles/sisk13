e099a94c5622419336464f930f9c5cdd30a4464a
Ariel Siles <ariel.siles@gmail.com>
2021-07-17 11:55:16 -0400

    query...

--- query/query_v5.2.4.sql
+++ query/query_v5.2.4.sql
@@ -8,3 +8,15 @@ UPDATE inv_almacenes a SET a.`tipo` = 'VETERINARY' WHERE a.`cod_alm` = 5;
 UPDATE inv_almacenes a SET a.`tipo` = 'AUXILIARY' WHERE a.`cod_alm` = 6;
 UPDATE inv_almacenes a SET a.`tipo` = 'REPLACEMENT' WHERE a.`cod_alm` = 7;
 UPDATE inv_almacenes a SET a.`tipo` = 'AGENCY' WHERE a.`cod_alm` = 8;
+
+--
+select *
+from ventadirecta v
+where v.`FECHA_PEDIDO` >= '2021-04-01'
+;
+
+select p.`IDPEDIDOS`, p.`DESCRIPCION`, p.`FECHA_ENTREGA`
+from pedidos p
+join articulos_pedido a on p.`DESCRIPCION` = a.`IDVENTADIRECTA`
+where p.`IDPEDIDOS` between 3628 and 3930
+;
