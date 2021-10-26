ebb91addeaeaa28cb389df014f8be278aa7427f2
borboton13 <borboton23@gmail.com>
2021-10-25 23:11:03 -0400

    HttpClient

---
+++ src/main/com/encens/khipus/util/RestHttpClient.java
@@ -0,0 +1,18 @@
+ package com.encens.khipus.util;
+
+
+/*import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.Response;*/
+
+ class RestHttpClient {
+
+
+     public static void main(String[] args){
+
+
+
+
+     }
+ }
