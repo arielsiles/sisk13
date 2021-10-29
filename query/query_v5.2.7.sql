b2ab3d43b850de60f01a8996617aafce5b70ab4c
borboton13 <borboton23@gmail.com>
2021-10-29 14:51:29 -0400

    v2.5.7 facturacion fin de mes

--- query/query_v5.2.7.sql
+++ query/query_v5.2.7.sql
@@ -1,7 +1,7 @@
 /**  29.10.2021  **/
 
 INSERT INTO funcionalidad(idfuncionalidad, codigo, idmodulo, permiso, nombrerecurso, idcompania)
-VALUES(429, 'CREDITSALE_INVOICING_END_MONTH', 1, 1, 'Functionality.customers.InvoicingEndOfMonth', 1);
+VALUES(429, 'CREDITSALE_INVOICING_END_MONTH', 1, 1, 'Functionality.customers.invoicingEndOfMonth', 1);
 
 -- => Crear Rol 'Facturacion fin de mes' y asignar al usuario breque
 
