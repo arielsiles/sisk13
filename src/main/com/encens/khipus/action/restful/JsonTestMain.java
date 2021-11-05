0cff68cc6485aecc9f78b2ce321af5a79f1523f0
borboton13 <borboton23@gmail.com>
2021-11-05 00:37:03 -0400

    Parsing Json with Jackson

---
+++ src/main/com/encens/khipus/action/restful/JsonTestMain.java
@@ -0,0 +1,23 @@
+package com.encens.khipus.action.restful;
+
+import com.fasterxml.jackson.databind.JsonNode;
+
+import java.io.IOException;
+
+public class JsonTestMain {
+
+    public static void main(String[] args){
+
+        String jsonSource = "{ \"title\": \"Coder from asdf\" }";
+
+        try {
+            JsonNode node = Json.parse(jsonSource);
+            System.out.println(node.get("title").asText());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
