8bd02e96e9934547a6ed60dcb94ffca9e1a3b1e6
borboton13 <borboton23@gmail.com>
2022-01-13 12:28:53 -0400

    Derechos de accedo SFE annul

---
+++ query/query_v5.4.1.sql
@@ -0,0 +1,20 @@
+INSERT INTO funcionalidad(idfuncionalidad, codigo, idmodulo, permiso, nombrerecurso, idcompania)
+VALUES(435, 'VALIDATE_NIT_SFE', 1, 1, 'Functionality.customers.validateNitSFE', 1);
+
+INSERT INTO funcionalidad(idfuncionalidad, codigo, idmodulo, permiso, nombrerecurso, idcompania)
+VALUES(436, 'PRINT_INVOICE_SFE', 1, 1, 'Functionality.customers.printInvoiceSFE', 1);
+
+INSERT INTO funcionalidad(idfuncionalidad, codigo, idmodulo, permiso, nombrerecurso, idcompania)
+VALUES(437, 'PRINT_DELIVERY_NOTE', 1, 1, 'Functionality.customers.printDeliveryNoteOrder', 1);
+
+INSERT INTO funcionalidad(idfuncionalidad, codigo, idmodulo, permiso, nombrerecurso, idcompania)
+VALUES(438, 'SEND_MAIL_INVOICE', 1, 1, 'Functionality.customers.sendMailInvoice', 1);
+
+INSERT INTO funcionalidad(idfuncionalidad, codigo, idmodulo, permiso, nombrerecurso, idcompania)
+VALUES(439, 'CHANGE_BILLING_METHOD_SFE', 1, 1, 'Functionality.customers.changeBillingMethodSFE', 1);
+
+INSERT INTO funcionalidad(idfuncionalidad, codigo, idmodulo, permiso, nombrerecurso, idcompania)
+VALUES(440, 'CONNECTION_TEST_SFE', 1, 1, 'Functionality.customers.connectionTestSFE', 1);
+
+INSERT INTO funcionalidad(idfuncionalidad, codigo, idmodulo, permiso, nombrerecurso, idcompania)
+VALUES(441, 'CANCEL_ORDER_INVOICE_SFE', 1, 1, 'Functionality.customers.cancelInvoiceOrderSFE', 1);
