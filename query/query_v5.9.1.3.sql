d2c37b81f640e02b5aa46220ea479d84c6507ddc
Ariel Siles <ariel.siles@gmail.com>
2024-01-25 18:41:01 -0400

    Actualizacion de datos P/Terdemol

--- query/query_v5.9.1.3.sql
+++ query/query_v5.9.1.3.sql
@@ -3,3 +3,6 @@ alter table configuracion add column cxp_provmn varchar(20);
 
 -- To Terdemol
 update configuracion set cxp_provmn = '21200101' where no_cia = '01';
+
+-- To ILVA
+update configuracion set cxp_provmn = '2420910300' where no_cia = '01';
