5f2ca7140aa69d2ed8ff09d144960728f03db6ee
Ariel Siles <ariel.siles@gmail.com>
2024-02-01 16:14:27 -0400

    Test, Save VoucherDetail Provider

--- src/main/com/encens/khipus/service/production/CollectMaterialServiceBean.java
+++ src/main/com/encens/khipus/service/production/CollectMaterialServiceBean.java
@@ -4,11 +4,6 @@ import com.encens.khipus.exception.finances.CompanyConfigurationNotFoundExceptio
 import com.encens.khipus.framework.action.Outcome;
 import com.encens.khipus.model.finances.*;
 import com.encens.khipus.model.production.CollectMaterial;
-import com.encens.khipus.model.production.CollectMaterialState;
-import com.encens.khipus.model.warehouse.InventoryMovement;
-import com.encens.khipus.model.warehouse.MovementDetail;
-import com.encens.khipus.model.warehouse.Warehouse;
-import com.encens.khipus.model.warehouse.WarehouseVoucher;
 import com.encens.khipus.service.accouting.VoucherAccoutingService;
 import com.encens.khipus.service.finances.FinanceProviderService;
 import com.encens.khipus.service.fixedassets.CompanyConfigurationService;
@@ -25,7 +20,9 @@ import org.jboss.seam.international.StatusMessage;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import java.math.BigDecimal;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 @Stateless
 @Name("collectMaterialService")
@@ -49,8 +46,7 @@ public class CollectMaterialServiceBean implements CollectMaterialService {
 
     @Override
     public List<CollectMaterial> findCollectMaterialNoAccounting(Date startDate, Date endDate) {
-        List<CollectMaterial> resultList = em.createQuery("select c from CollectMaterial as c \n" +
-
+        List<CollectMaterial> resultList = em.createQuery("select c from CollectMaterial c " +
                         "where c.date between :startDate and :endDate and c.state != 'PEN' and c.accountigFlag = :state")
                         .setParameter("startDate", startDate)
                         .setParameter("endDate", endDate)
@@ -73,27 +69,32 @@ public class CollectMaterialServiceBean implements CollectMaterialService {
         FinancesEntity financesEntity;
 
         for (CollectMaterial colMat:collectMaterialList) {
-            financesEntity =financeProviderService.findByIdNumber(colMat.getProducer().getIdNumber());
+            financesEntity = financeProviderService.findByIdNumber(colMat.getProducer().getIdNumber());
+            System.out.println("----> financesEntity: " + financesEntity.getIdNumber());
+
             CashAccount warehouseCashAccount = colMat.getMetaProduct().getProductItem().getWarehouse().getWarehouseCashAccount();
-            BigDecimal aux= BigDecimalUtil.divide(colMat.getBalanceWeight(),BigDecimalUtil.toBigDecimal(1000));
-                    BigDecimal amount = BigDecimalUtil.multiply(aux, colMat.getPrice());
+            BigDecimal aux = BigDecimalUtil.divide(colMat.getBalanceWeight(),BigDecimalUtil.toBigDecimal(1000));
+            BigDecimal amount = BigDecimalUtil.multiply(aux, colMat.getPrice());
+
             VoucherDetail voucherDetailDev = VoucherDetailBuilder.newDebitVoucherDetail(
                     null, null, warehouseCashAccount, amount, FinancesCurrencyType.P, BigDecimal.ONE);
+
             voucherDetailDev.setQuantityArt(colMat.getBalanceWeight());
             voucherDetailDev.setProductItemCode(colMat.getMetaProduct().getProductItem().getProductItemCode());
             voucher.getDetails().add(voucherDetailDev);
 
-
-            CashAccount cashAccountDev = companyConfiguration.getGeneralCashAccountNational();
             VoucherDetail supplierAccountOutput = VoucherDetailBuilder.newCreditVoucherDetail(
                     null, null, companyConfiguration.getAccountPayableSupplier(), amount, FinancesCurrencyType.P, BigDecimal.ONE);
-            supplierAccountOutput.setProviderCode(financesEntity.getIdNumber());
-            supplierDetailCashAcounts.add(supplierAccountOutput);
 
+            voucher.getDetails().add(supplierAccountOutput);
+
+            supplierAccountOutput.setProviderCode(financesEntity.getId().toString());
+            //supplierDetailCashAcounts.add(supplierAccountOutput);
+            //voucher.getDetails().add(voucherDetailDev);
 
         }
 
-        Collections.sort(supplierDetailCashAcounts, new Comparator<VoucherDetail>() {
+        /*Collections.sort(supplierDetailCashAcounts, new Comparator<VoucherDetail>() {
             @Override
             public int compare(VoucherDetail o1, VoucherDetail o2) {
                 return o1.getAccount().compareTo(o2.getAccount());
@@ -102,7 +103,8 @@ public class CollectMaterialServiceBean implements CollectMaterialService {
 
         for (VoucherDetail voucherDetail:supplierDetailCashAcounts){
             voucher.getDetails().add(voucherDetail);
-        }
+        }*/
+
         voucher.setDate(new Date());
         voucherAccoutingService.saveVoucher(voucher);
         result = Outcome.SUCCESS;
