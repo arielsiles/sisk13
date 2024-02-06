917bb38dab3b349818d03a6afbc1a4f481536f1f
fznumber <diego.loza.click@gmail.com>
2024-02-05 23:09:56 -0400

    nuevo modulo añadido en la tabala module menu superior de incio clonado pero sin permisos

---
+++ query/query_v6.0.0.sql
@@ -0,0 +1,4 @@
+--nuevo modulo de produccion de terdemol
+INSERT INTO ilva.modulo (idmodulo, descripcion, nombrerecurso, idcompania)
+VALUES (11, 'modulo de produccion de TERDEMOL', 'productionx', 1);
+
