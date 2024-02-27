b6695b5716547ab72318d4c518cf92023a257550
Ariel Siles <ariel.siles@gmail.com>
2024-02-27 10:22:05 -0400

    v6.0.3 roles de acceso

---
+++ query/query_v6.0.3.sql
@@ -0,0 +1,11 @@
+-- 26.02.2024
+INSERT INTO funcionalidad(idfuncionalidad, codigo, idmodulo, permiso, nombrerecurso, idcompania)
+VALUES(293, 'GENERAL_INVENTORY_REPORT', 5, 1, 'menu.warehouse.report.inventoryReport', 1);
+
+INSERT INTO funcionalidad(idfuncionalidad, codigo, idmodulo, permiso, nombrerecurso, idcompania)
+VALUES(294, 'PRODUCT_MOVEMENT_REPORT', 5, 1, 'Reports.kardex.movement.menu.title', 1);
+
+INSERT INTO funcionalidad(idfuncionalidad, codigo, idmodulo, permiso, nombrerecurso, idcompania)
+VALUES(295, 'VALUED_PHYSICAL_INVENTORY_REPORT', 5, 1, 'menu.warehouse.report.valuedPhysicalInventory', 1);
+
+
