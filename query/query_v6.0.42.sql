2b7da2097ad94ddf0229d28b32a75577c69f0b9c
Ariel Randy <ariel.siles@gmail.com>
2024-12-04 01:46:08 -0400

    Release v6.0.42 Contabilizar acopio por volqueta

--- query/query_v6.0.42.sql
+++ query/query_v6.0.42.sql
@@ -1,3 +1,9 @@
 -- 03.12.2024
 alter table acopiomp add column pesoprov decimal(12, 2) after codigo;
 update acopiomp set pesoprov = 0 where pesoprov is null;
+
+update acopiomp set pesoprov = pesoneto;
+update acopiomp set pesoneto = pesobal;
+
+insert into `_sequence` (seq_name, seq_val) values ('IMP', 0);
+insert into `tipodoc` (IDTIPODOC, NOMBRE, DESCRIPCION) values (15, 'IMP', 'INGRESO DE MATERIA PRIMA');
